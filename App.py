dc0159a0ac1f51a767c40a5e43103ea5a5d3fcde
Yashraj Kakkad <yashrajkakkad@gmail.com>
2019-11-22 12:17:38 +0530

    Removed unnecessary files

--- App.py
+++
@@ -1,7 +0,0 @@
-from utils.Graph import gen_recommendations
-
-
-# Get nodes from form
-def gen_results(nodes):
-    union_colors_results, energy_spread_results = gen_recommendations(nodes)
-    return union_colors_results, energy_spread_results
