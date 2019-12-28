eac8fb272e9a0b1a8a8aa9a21e8bb2b539f375d6
Yashraj Kakkad <yashrajkakkad@gmail.com>
2019-12-28 18:02:21 +0530

    Updated ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!e_@1-%lz+v+u5y$hdndg5w17wgatlu1443dty%wcmmo&47rlj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '192.241.249.93']
 
 # Application definition
 
